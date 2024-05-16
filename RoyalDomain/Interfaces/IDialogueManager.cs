5999970979947750506a473fef4f083ed829d1ee
Shacklebottom <last0illusion@gmail.com>
2024-05-15 20:27:32 -0500

    added IDialogueManager, DialogueManager, and DialogueManagerTests

---
+++ RoyalDomain/Interfaces/IDialogueManager.cs
@@ -0,0 +1,9 @@
+﻿using RoyalDomain.Objects;
+
+namespace RoyalDomain.Interfaces
+{
+    public interface IDialogueManager
+    {
+        List<string> GetFileInfo(string filePath);
+    }
+}
---
+++ RoyalDomain/Objects/DialogueManager.cs
@@ -0,0 +1,13 @@
+﻿using RoyalDomain.Interfaces;
+
+namespace RoyalDomain.Objects
+{
+    public class DialogueManager() : IDialogueManager
+    {
+        public List<string>GetFileInfo(string filePath)
+        {
+            return new List<string> { filePath };
+            //return File.ReadAllLines(filePath).ToList();
+        }
+    }
+}
